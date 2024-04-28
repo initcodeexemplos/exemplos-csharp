95af648872e7fdb9da3cdac264021a6cc1ba0c19
Renan <warreir@gmail.com>
2024-04-28 20:33:16 -0300

    melhorando o código

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello World Renan");
+﻿namespace programa {
+    public class Program {
+        static void Main(){
+            int num, num2;
+
+            Console.WriteLine("Digite um número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite outro número: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("O valor digitado foi " + (num + num2));
+            Console.WriteLine($"O valor digitado foi {num + num2}");
+        }
+    }
+}
